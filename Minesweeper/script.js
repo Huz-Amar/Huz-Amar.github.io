938acf4349ea7cb22ba9ed820c66ef7db1cf44a7
Huz-Amar <35269412+Huz-Amar@users.noreply.github.com>
2020-10-07 01:49:21 -0600

    trying to get action listener working

--- Minesweeper/script.js
+++ Minesweeper/script.js
@@ -1,1 +1,39 @@
-o
+// size of board
+const SIZE = 3;
+
+// button array and struct
+let buttonArray = []
+for (let i = 0; i < SIZE; i++) {
+    for (let j = 0; j < SIZE; j++) {
+        buttonArray[i] = [];
+        buttonArray[i][j] = {
+            isMine: false,
+            neighbouringMines: 0
+        }
+    }
+}
+
+window.addEventListener('load', main);
+
+function main() {
+    console.table(buttonArray);
+    let grid = document.getElementById("grid");
+    createButtons(grid);
+}
+
+function createButtons(grid) {
+    for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE; j++) {
+            let btn = document.createElement("button", 'class="btn btn-secondary"');
+            btn.className = "btn btn-secondary";
+            btn.innerHTML = "Btn";
+            btn.addEventListener("click", () => console.log("Working"));
+            grid.appendChild(btn);
+        }
+        grid.innerHTML += "<br>";
+    }
+}
+
+function callback() {
+    alert("Callback working");
+}
